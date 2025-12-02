921e421e1aeefa34c19c4563bd2fd6ee5167d751
Leonardo Padilha Fontana <leonardopad2@gmail.com>
2025-12-02 00:04:47 -0300

    Adicionando bootstrap nos fontes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
-import './bootstrap';
+import 'bootstrap';
+// Se precisar do CSS separado no seu JS, você pode importar aqui também:
+// import 'bootstrap/dist/css/bootstrap.min.css';
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
     <title>{{$title}}</title>
 </head>
 <body>
+<div>
     <h1>{{$title}}</h1>
-
+    
     {{ $slot }}
+</div>
 </body>
 </html>
